e19a8cdc20efda73ff8d8b10cbb2b4bfc42e2a54
MajjiBindu <bindumadhavi6281@gmail.com>
2025-03-08 22:18:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     let changeTitleButton = document.createElement("button");
+//     changeTitleButton.textContent = "Change Page Title";
+//     document.body.appendChild(changeTitleButton);
+  
+//     changeTitleButton.addEventListener('click', function() {
+//       document.title = "New Title: Photography Website";
+//       alert("Page title changed!");
+//     });
+//   });
+  
